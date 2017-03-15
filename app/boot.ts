4e42030a9f87381d26ef6052a1949a6de5f60e20
mbpopova <mpopova.us@gmail.com>
2017-03-15 13:06:37 -0400

    Add files via upload

--- app/boot.ts
+++ app/boot.ts
@@ -1,5 +1,4 @@
 import {bootstrap}    from 'angular2/platform/browser'
-import {AppComponent} from './app.component'
 import {StudentappComponent} from './studentapp.component'
 import {ROUTER_PROVIDERS} from 'angular2/router'
 
